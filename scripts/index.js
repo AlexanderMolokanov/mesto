78123246a4f8f0c962490b2e5f02e7e5828e3d0d
AlexanderMolokanov <g.mkv@yandex.ru>
2022-01-04 22:54:59 +0300

    сломать редактор формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,66 @@
-const buttonProfileEdit = document.querySelector( '.profile__edit');
+const buttonProfileEdit = document.querySelector('.profile__edit');
 // const buttonProfileAdd = document.querySelector(selectors: '.profile__add');
-// const editButtonSave = document.querySelector(selectors: '.edit__button-save');
-const editButtonClose = document.querySelector( '.edit__button-close');
-const edit = document.querySelector('.edit');
-const editButtonActiveClass = 'edit_active';
+// const popupButtonSave = document.querySelector(selectors: '.edit__button-save');
+const popupButtonClose = document.querySelector('.popup__button-close');
+const popup = document.querySelector('.popup');
+const popupButtonActiveClass = 'popup_opened';
 
-buttonProfileEdit.addEventListener( 'click', function() {
-edit.classList.add(editButtonActiveClass);    
+const elementHeart = document.querySelectorAll('.element__heart');
+const elementHeartBlack = document.querySelector('.element__heart_black');
+const elementHeartBlackClass = 'element__heart_black';
+
+function logCharacters(elementHeart) {
+    for (let index = 0; index < elementHeart.length; index += 1) {
+        //console.log(elementHeart[index]);
+        elementHeart[index].addEventListener('click', function () {
+            elementHeart[index].classList.toggle(elementHeartBlackClass);
+        });
+    }
+}
+
+logCharacters(elementHeart)
+
+function openPopup() {
+    popup.classList.add(popupButtonActiveClass);
+    document.body.style.overflow = 'hidden';
+}
+
+function closePopup() {
+    popup.classList.remove(popupButtonActiveClass);
+    document.body.style.overflow = '';
+}
+
+// elementHeartBlack.addEventListener('click', function () {
+//     elementHeart.classList.remove(elementHeartBlackClass);
+// });
+
+buttonProfileEdit.addEventListener('click', function () {
+    openPopup();
 });
 
-editButtonClose.addEventListener( 'click', function() {
-edit.classList.remove(editButtonActiveClass);
+popupButtonClose.addEventListener('click', function () {
+    closePopup();
 });
+
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('#name-input');
+let jobInput = document.querySelector('#job-input');
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    nameInput.value;
+    jobInput.value;
+
+    const profileTitle = document.querySelector('.profile__title');
+    const profileSubtitle = document.querySelector('.profile__subtitle');
+
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopup();
+};
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
