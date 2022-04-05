2a379f17ab42fa4cf4e62b7982d8773ee6f74019
AlexanderMolokanov <91531787+AlexanderMolokanov@users.noreply.github.com>
2022-04-05 08:14:55 +0300

    ПР8 сделать класс Секшн

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,20 +1,19 @@
 export class Section {
-    constructor(items, renderer) {
-        this._name = card.name;
-        this._link = card.link;
-        this._cardsTemplate = document.querySelector(cardTemplateSelector).content;
-        this._cardElement = this._cardsTemplate.querySelector('.element').cloneNode(true);
-        this._like = this._cardElement.querySelector('.element__heart');
-        this._image = this._cardElement.querySelector('.element__image');
-        this._trash = this._cardElement.querySelector('.element__delete');
-
-        this._imageLink = document.querySelector("#popup-image-img");
-        this._imageCaption = document.querySelector(".popup__image-caption");
+    constructor({ items, renderer }, containerClass) {
+        this._items = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(containerSelector);
     }
 
-    _openBigImage() {
-        this._imageLink.src = this._link;
-        this._imageLink.alt = 'Изображение:' + ' ' + this._name;
-        this._imageCaption.textContent = this._name;
-        openPopup(bigImagePopup);
+    renderItem() {
+        this._items.forEach(element => {
+            const item = this._renderer(element);
+            this.addItem(item);
+        });
     }
+
+    addItem(element) {
+        this._container.prepend(element)
+    }
+
+}
