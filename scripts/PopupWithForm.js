7b70ac2f2cd893e67569658cb847bb89ece85903
AlexanderMolokanov <91531787+AlexanderMolokanov@users.noreply.github.com>
2022-04-17 22:58:27 +0300

    далать классы ПР8

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,6 +1,6 @@
 import Popup from './Popup.js';
 export default class PopupWithForm extends Popup {
-    constructor(popupSelector, formProfile) {
+    constructor(popupSelector, formProfile, formSubmitHandler) {
         super(popupSelector);
         this._formProfile = document.querySelector(formProfile);
     }
@@ -15,64 +15,13 @@ export default class PopupWithForm extends Popup {
 
     setEventListeners() {
         super.setEventListeners();
-        добавить обработчик сабмита формы
-
         this._formProfile.addEventListener('submit', (evt) => {
             evt.preventDefault();
-            profileTitle.textContent = profileEditInput.value;
-            profileSubtitle.textContent = profileEditJob.value;
-            closePopup(profilePopup);
-        };
+            this.formSubmitHandler(this._getInputValues());
+        });
     }
 
-    Метод setEventListeners класса PopupWithForm должен не только добавлять
-    обработчик клика иконке закрытия,
-    но и добавлять обработчик сабмита формы.
-
     close() {
-        Перезаписывает родительский метод close, так как при закрытии попапа форма должна ещё и сбрасываться.
-        Для каждого попапа создавайте свой экземпляр класса PopupWithForm.
-        
+        this._formProfile.reset();
     }
-}
-
-Создайте класс PopupWithForm, который наследует от Popup.
-Кроме селектора попапа принимает в конструктор колбэк сабмита формы.
-    Содержит
-приватный метод _getInputValues, который собирает данные всех полей формы.
-Перезаписывает родительский метод setEventListeners.
-
-
-
-
-import Popup from "./Popup.js";
-
-export default class PopupWithForm extends Popup {
-    constructor(dataSelectors, submitHandler) {
-        super(dataSelectors.popup);
-        this._form = this.popup.querySelector(dataSelectors.formSelector);
-        this._inputList = Array.from(this._form.querySelectorAll(dataSelectors.inputSelector));
-        this._submitHandler = submitHandler;
-    };
-
-    _getInputValues() {
-        this._formValues = {};
-        this._inputList.forEach((input) => {
-            this._formValues[input.name] = input.value;
-        });
-        return this._formValues;
-    };
-
-    setEventListenersForm() {
-        super.setEventListeners();
-        this._form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            this._submitHandler(this._getInputValues());
-        });
-    };
-
-    close() {
-        super.close();
-        this._form.reset();
-    };
 }
